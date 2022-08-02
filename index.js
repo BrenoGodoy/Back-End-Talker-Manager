dc445f7c62f5a7c82bdbe53535d281df34556934
Breno Godoy <godoyy25z@gmail.com>
2022-08-02 19:09:57 -0300

    fix: refatora requisito 1 e deixa o código mais legível

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const people = require('./talker.json');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const mainFile = './talker.json';
+const verifyEmpty = async (req, res, next) => {
+  const data = await fs.readFile(mainFile);
+  const parsedData = JSON.parse(data);
+  if (parsedData === []) return res.status(200).json([]);
+  next();
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (req, res, next) => {
-  if (people === '[]') {
-    return res.status(200).send([]);
-  }
-
-  next();
-}, 
-(req, res) => {
-  res.status(200).json(people);
+app.get('/talker', verifyEmpty, async (req, res) => {
+  const data = await fs.readFile(mainFile);
+  const parsedData = JSON.parse(data);
+  res.status(200).json(parsedData);
 });
 
 app.listen(PORT, () => {
