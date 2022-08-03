c7620a885b62c0d3b2744b3e52afa6892da51467
Breno Godoy <godoyy25z@gmail.com>
2022-08-03 17:01:47 -0300

    feat: Requisito 5 - chama método POST na rota /talker que adiciona um palestrante a lista de pessoas

--- index.js
+++ index.js
@@ -47,6 +47,54 @@ const verifyLogin = async (req, res, next) => {
   next();
 };
 
+// Middlewares PeopleToAdd
+
+const verifyPeopleToAdd1 = (req, res, next) => {
+  const { authorization } = req.headers;
+  const { name } = req.body;
+
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
+  }
+  next();
+};
+
+const verifyPeopleToAdd2 = (req, res, next) => {
+  const { age, talk } = req.body;
+
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(age) <= 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' }); 
+  if (!talk.watchedAt) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  next();
+};
+
+const verifyPeopleToAdd3 = (req, res, next) => {
+  const { talk } = req.body;
+  const regex = new RegExp(/\w\w\/\w\w\/\w\w\w\w/);
+
+  console.log(regex.test(talk.watchedAt));
+  if (!regex.test(talk.watchedAt)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (!talk.rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+// fim dos Middlewares PeopleToAdd
+
 // https://www.delftstack.com/pt/howto/javascript/javascript-random-string/
 const generateRandomString = (num) => {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -77,6 +125,19 @@ app.get('/talker/:id', verifyId, async (req, res) => {
   const parsedData = JSON.parse(data);
   const people = parsedData.find((p) => p.id === Number(id));
   res.status(200).json(people);
+});
+
+app.post('/talker', 
+[verifyPeopleToAdd1, verifyPeopleToAdd2, verifyPeopleToAdd3], async (req, res) => {
+  const { name, age, talk } = req.body;
+  const data = await fs.readFile(mainFile);
+  const parsedData = JSON.parse(data);
+
+  const id = parsedData.length + 1;
+  const peopleToAdd = { id, name, age, talk };
+  const newData = [...parsedData, peopleToAdd];
+  await fs.writeFile('./talker.json', JSON.stringify(newData));
+  res.status(201).json(peopleToAdd); 
 });
 
 app.post('/login', verifyLogin, (req, res) => {
